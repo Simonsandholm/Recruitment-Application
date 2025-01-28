2fed0e9aeaca0884278395238e21412e17dd3900
Goooooor <gor.hukasjan@hotmail.com>
2025-01-28 12:49:53 +0100

    Integrated db and added domain entities

--- src/main/java/se/kth/iv1201/group4/recruitment/recruitmentapp/RecruitmentApplication.java
+++ src/main/java/se/kth/iv1201/group4/recruitment/recruitmentapp/RecruitmentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 // Temporarily disabled security and db management
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, SecurityAutoConfiguration.class})
+@SpringBootApplication
 public class RecruitmentApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/se/kth/iv1201/group4/recruitment/recruitmentapp/domain/Availability.java
@@ -0,0 +1,20 @@
+package se.kth.iv1201.group4.recruitment.recruitmentapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Availability {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
